6265469014e2f1e9e38061ec14af8dd52032dcdb
unknown <yjw1555@gmail.com>
2025-10-07 18:55:00 +0900

    pc finished

--- script.js
+++ script.js
@@ -1,47 +1,48 @@
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-const loadingPage = document.querySelector(".loading-page");
-const loadingImg = loadingPage.querySelector("#loading-image");
-let loadingImgArr = [
-  "image/loading_screen/intro_img01.png",
-  "image/loading_screen/intro_img02.png",
-  "image/loading_screen/intro_img03.png",
-  "image/loading_screen/intro_img04.png",
-  "image/loading_screen/intro_img05.png",
-  "image/loading_screen/intro_img06.png",
-  "image/loading_screen/intro_img07.png",
-  "image/loading_screen/intro_img08.png",
-  "image/loading_screen/intro_img09.png",
-  "image/loading_screen/intro_img10.png",
-  "image/loading_screen/intro_img11.png",
-  "image/loading_screen/intro_img12.png",
-  "image/loading_screen/intro_img13.png",
-  "image/loading_screen/intro_img14.png",
-  "image/loading_screen/intro_img15.png",
-  "image/loading_screen/intro_img16.png",
-];
-let imgArrLength = loadingImgArr.length;
-for (let i = 0; i < imgArrLength; i++) {
+function loading__init() {
+  const loadingPage = document.querySelector(".loading-page");
+  const loadingImg = loadingPage.querySelector("#loading-image");
+  let loadingImgArr = [
+    "image/loading_screen/intro_img01.png",
+    "image/loading_screen/intro_img02.png",
+    "image/loading_screen/intro_img03.png",
+    "image/loading_screen/intro_img04.png",
+    "image/loading_screen/intro_img05.png",
+    "image/loading_screen/intro_img06.png",
+    "image/loading_screen/intro_img07.png",
+    "image/loading_screen/intro_img08.png",
+    "image/loading_screen/intro_img09.png",
+    "image/loading_screen/intro_img10.png",
+    "image/loading_screen/intro_img11.png",
+    "image/loading_screen/intro_img12.png",
+    "image/loading_screen/intro_img13.png",
+    "image/loading_screen/intro_img14.png",
+    "image/loading_screen/intro_img15.png",
+    "image/loading_screen/intro_img16.png",
+  ];
+  let imgArrLength = loadingImgArr.length;
+  for (let i = 0; i < imgArrLength; i++) {
+    setTimeout(() => {
+      loadingImg.src = loadingImgArr[i];
+    }, i * 300);
+  }
+
+  let loadingTime = imgArrLength * 300;
+
+  const percentage = loadingPage.querySelector(".percentage--number");
+  percentage.textContent = "0";
+
+  for (let i = 0; i <= 100; i++) {
+    setTimeout(() => {
+      percentage.textContent = i;
+    }, i * 45);
+  }
+
   setTimeout(() => {
-    loadingImg.src = loadingImgArr[i];
-  }, i * 300);
+    loadingPage.classList.remove("active");
+  }, loadingTime);
 }
-
-let loadingTime = imgArrLength * 300;
-
-const percentage = loadingPage.querySelector(".percentage--number");
-percentage.textContent = "0";
-
-for (let i = 0; i <= 100; i++) {
-  setTimeout(() => {
-    percentage.textContent = i;
-  }, i * 45);
-}
-
-setTimeout(() => {
-  loadingPage.classList.remove("active");
-  mainSwiper__init();
-}, loadingTime);
 
 function mainSwiper__init() {
   const sec1SwiperEl = document.querySelector(".section-1 .swiper-container");
@@ -85,7 +86,6 @@ function sec2Gsap__init() {
     }
   );
 }
-sec2Gsap__init();
 
 function sec3Gsap__init() {
   const target = document.querySelectorAll(".section-3 .portfolio .port-item");
@@ -99,30 +99,29 @@ function sec3Gsap__init() {
       defaultWidth: isMobile ? innerWidth * 0.6 : "34rem",
     };
   }
-  let { activeWidth, defaultWidth } = getWidth();
 
   gsapActive();
 
   window.addEventListener("resize", () => {
-    ({ activeWidth, defaultWidth } = getWidth());
-    ScrollTrigger.update();
+    ScrollTrigger.getAll().forEach((el) => {
+      if (el.trigger && el.trigger.classList.contains("port-item")) {
+        el.kill();
+      }
+    });
+    ScrollTrigger.refresh(true);
+    gsapActive();
   });
 
   function gsapActive() {
     target.forEach((portCard) => {
-      let tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: portCard,
-          start: "top 70%",
-          end: "bottom 30%",
-          scrub: 1,
-        },
+      let tl = gsap.timeline();
+      gsap.set(portCard, {
+        width: () => getWidth().defaultWidth,
       });
-
       tl.to(
         portCard,
         {
-          width: activeWidth,
+          width: () => getWidth().activeWidth,
           ease: "none",
           duration: 0.3,
         },
@@ -130,17 +129,23 @@ function sec3Gsap__init() {
       ).to(
         portCard,
         {
-          width: defaultWidth,
+          width: () => getWidth().defaultWidth,
           ease: "none",
           duration: 0.3,
         },
         0.9
       );
+
+      let st = ScrollTrigger.create({
+        trigger: portCard,
+        start: "top 70%",
+        end: "bottom 30%",
+        scrub: 1,
+        animation: tl,
+      });
     });
   }
 }
-
-sec3Gsap__init();
 
 // original source code from original site !!
 function sec3GsapAlt__init() {
@@ -202,3 +207,72 @@ function sec3GsapAlt__init() {
   });
 }
 // sec3GsapAlt__init();
+
+function sec4Gasp__init() {
+  const ScrollTarget = document.querySelector(".section-4");
+  const aniTarget = ScrollTarget.querySelector(".item-box-2");
+
+  let tl = gsap.timeline();
+  tl.fromTo(aniTarget, { y: -150 }, { y: 150, ease: "none", duration: 10 });
+
+  let st = ScrollTrigger.create({
+    trigger: ScrollTarget,
+    animation: tl,
+    scrub: 3,
+  });
+}
+
+function marqueeWidth() {
+  const marquee = document.querySelector(".marquee-container");
+  const marqueeWrap = marquee.querySelector(".marquee-wrap");
+  const marqueeSlide = marqueeWrap.querySelector(".marquee-slide");
+  let width = marqueeSlide.getBoundingClientRect().width;
+
+  const marqueeAni = [{ transform: `translateX(0)` }, { transform: `translateX(-${width}px)` }];
+  let duration = 10000;
+  const marqueeTiming = {
+    duration: duration,
+    iterations: Infinity,
+    easing: "linear",
+  };
+
+  marqueeAnimationInstance = marqueeWrap.animate(marqueeAni, marqueeTiming);
+
+  marqueeWrap.addEventListener("pointerenter", () => {
+    marqueeAnimationInstance.playbackRate = 0.3;
+    console.log("hi");
+  });
+  marqueeWrap.addEventListener("pointerleave", () => {
+    marqueeAnimationInstance.playbackRate = 1;
+    console.log("hi");
+  });
+}
+
+function footerMenuSwap() {
+  const footerMenuEl = document.querySelectorAll(".footer .overflow-box > span");
+
+  footerMenuEl.forEach((el) => {
+    let content = el.textContent;
+    el.style.setProperty("--content", `"${content}"`);
+  });
+}
+
+ScrollSmoother.create({
+  wrapper: "#smooth-wrapper",
+  content: "#smooth-content",
+  smooth: 1,
+  effects: true,
+});
+
+window.addEventListener("load", () => {
+  loading__init();
+
+  setTimeout(() => {
+    footerMenuSwap();
+    marqueeWidth();
+    sec4Gasp__init();
+    sec2Gsap__init();
+    sec3Gsap__init();
+    mainSwiper__init();
+  }, 5000);
+});
